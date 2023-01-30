d270360bb0c3f572be9fa6859c99998f15a17e9a
Vũ Sinh Huy <46261472+vshuy@users.noreply.github.com>
2023-01-30 22:20:53 +0700

    update cors origin
    update cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*','https://niklab.netlify.app'],
 
     'allowed_origins_patterns' => [],
 
